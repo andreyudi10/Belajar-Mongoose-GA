d74a2c2cd13b64f3883b38a70c895348290a8e33
andreyudi10 <andrianus.yudi@gmail.com>
2021-05-25 22:27:23 +0700

    adding commentary

--- routes/user.js
+++ routes/user.js
@@ -6,22 +6,29 @@ const router = new express.Router();
 //mirip denga react-router-dom di react
 
 router.post('/users', async (req, res) => {
+    // async karena dari sananya mongodb async
     const user = new User(req.body);
-    //bikin blueprint dari skema user lalu dia kirim user
+    //bikin blueprint dari skema User lalu dia kirim user
     try {
         await user.save();
+        // maksudnya pake user.save() itu bagaimana ya?
+        // kenapa pake await baca dokumentasi aga bingung2
+        // https://mongoosejs.com/docs/promises.html 
         res.status(201).send(user);
+        // gunakan res (variabel kedua) dan kasih status 201 kemudian kirim objek user
     } catch (err) {
         res.send(400).send(err);
+        // jika eror kirim dengan status 400 dan masukin status err nya apa.
     }
 })
 
 router.get('/users', async (req, res) => {
     try {
         const users = await User.find({});
+        // nunggu fungsi find selese jalan baru dia res.send(users) ya
         res.send(users)
     } catch (err) {
-        res.send(500).send();
+        res.send(500).send(err);
     }
 })
 
@@ -39,8 +46,10 @@ router.get('/users/:id', async (req, res) => {
 })
 
 router.patch('/users/:id', async (req, res) => {
+    // pelajarin patch
     const updates = Object.keys(req.body);
     const allowedUpdates = ['name', 'email', 'password', 'age'];
+    //yang diperbolehkan update
     const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
     if(!isValidOperation) {
         return res.status(400).send({ error: 'Invalid updates!'});
